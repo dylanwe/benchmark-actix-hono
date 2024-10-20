8e8c9198484bff6797aff4f0b4a9d4b962ad149d
Dylan Weijgertze <dylanwprivate@gmail.com>
2024-10-20 16:25:05 +0200

    feat: add stages

--- k6/script.js
+++ k6/script.js
@@ -1,51 +1,31 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const MAX_CLIENTS = 100;
+const MIN_CLIENTS = 1;
+const STAGE_INTERVAL = '2m';
+
+function setupStages() {
+  const stages = []
+
+  for (let i = MIN_CLIENTS; i <= MAX_CLIENTS; i++) {
+    stages.push({
+      target: i + 1,
+      duration: STAGE_INTERVAL,
+    });
+  }
+
+  return stages;
+}
+
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 1,
-  // A string specifying the total duration of the test run.
-  duration: '2m',
-
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud.
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
-  // cloud: {
-  //   // The ID of the project to which the test is assigned in the k6 Cloud UI.
-  //   // By default tests are executed in default project.
-  //   projectID: "",
-  //   // The name of the test in the k6 Cloud UI.
-  //   // Test runs with the same name will be grouped.
-  //   name: "script.js"
-  // },
-
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'shared-iterations',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // }
+  scenarios: {
+    test: {
+      executor: 'ramping-vus',
+      startVUs: MIN_CLIENTS,
+      stages: setupStages(),
+    },
+  },
 };
 
 // The function that defines VU logic.
@@ -55,4 +35,5 @@ export const options = {
 //
 export default function() {
   http.get('http://192.168.68.148:30008/api/v1/submarine');
+  http.get('http://192.168.68.148:30009/api/v1/submarine');
 }
