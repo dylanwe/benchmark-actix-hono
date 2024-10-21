376456d98ea4f34d8d188c0743b0062d61ce01d9
Dylan Weijgertze <dylanwprivate@gmail.com>
2024-10-21 13:22:11 +0200

    fix: seperate two loads and run with timeout

--- k6/script.js
+++ k6/script.js
@@ -1,16 +1,20 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-const MAX_CLIENTS = 100;
-const MIN_CLIENTS = 1;
-const STAGE_INTERVAL = '2m';
+const MAX_CLIENTS = 1000;
+const MIN_CLIENTS = 20;
+const STAGE_INTERVAL = '30s';
+const REQUEST_OPTIONS = {
+  timeout: '200ms',
+};
 
 function setupStages() {
   const stages = []
+  const CLIENTS_TO_ADD = 10;
 
-  for (let i = MIN_CLIENTS; i <= MAX_CLIENTS; i++) {
+  for (let i = MIN_CLIENTS; i <= (MAX_CLIENTS / CLIENTS_TO_ADD); i++) {
     stages.push({
-      target: i + 1,
+      target: (i + 1) * CLIENTS_TO_ADD,
       duration: STAGE_INTERVAL,
     });
   }
@@ -20,20 +24,28 @@ function setupStages() {
 
 export const options = {
   scenarios: {
-    test: {
+    deno: {
       executor: 'ramping-vus',
       startVUs: MIN_CLIENTS,
       stages: setupStages(),
+      exec: 'denoTest',
+    },
+    actix: {
+      executor: 'ramping-vus',
+      startVUs: MIN_CLIENTS,
+      stages: setupStages(),
+      exec: 'actixTest',
     },
   },
 };
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
-//
-export default function() {
-  http.get('http://192.168.68.148:30008/api/v1/submarine');
-  http.get('http://192.168.68.148:30009/api/v1/submarine');
+export function denoTest() {
+  http.get('http://192.168.68.148:30008/api/v1/submarine', REQUEST_OPTIONS);
+  sleep(0.02);
 }
+
+export function actixTest() {
+  http.get('http://192.168.68.148:30009/api/v1/submarine', REQUEST_OPTIONS);
+  sleep(0.02);
+}
+
