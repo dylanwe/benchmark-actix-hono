78ce9f9b4a436dbb4eabf4864aa40a504de72834
Dylan Weijgertze <dylanwprivate@gmail.com>
2024-11-02 13:39:04 +0100

    feat: add complex endpoint

--- k6/script.js
+++ k6/script.js
@@ -1,51 +1,50 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-const MAX_CLIENTS = 620;
-const MIN_CLIENTS = 20;
-const STAGE_INTERVAL = '2m';
+const MAX_CLIENTS = 200;
+const STAGE_DURATION = '2h';
 const REQUEST_OPTIONS = {
   timeout: '200ms',
 };
-
-function setupStages() {
-  const stages = [];
-  const iterations = Math.floor(MAX_CLIENTS / MIN_CLIENTS);
-
-  for (let i = 0; i < iterations; i++) {
-    stages.push({
-      target: i * MIN_CLIENTS,
-      duration: STAGE_INTERVAL,
-    });
-  }
-
-  return stages;
-}
+const COMPLEX_REQUEST_BODY = JSON.stringify({
+  instructions: "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2\n"
+});
 
 export const options = {
   scenarios: {
     actix: {
       executor: 'ramping-vus',
-      startVUs: MIN_CLIENTS,
-      stages: setupStages(),
-      exec: 'actixTest',
+      stages: [
+        { duration: STAGE_DURATION, target: MAX_CLIENTS },
+      ],
+      exec: 'complexActixTest',
     },
     hono: {
       executor: 'ramping-vus',
-      startVUs: MIN_CLIENTS,
-      stages: setupStages(),
-      exec: 'honoTest',
+      stages: [
+        { duration: STAGE_DURATION, target: MAX_CLIENTS },
+      ],
+      exec: 'complexHonoTest',
     },
   },
 };
 
-export function honoTest() {
+export function simpleHonoTest() {
   http.get('http://192.168.68.148:30008/api/v1/submarine', REQUEST_OPTIONS);
   sleep(0.02);
 }
 
-export function actixTest() {
+export function simpleActixTest() {
   http.get('http://192.168.68.148:30009/api/v1/submarine', REQUEST_OPTIONS);
   sleep(0.02);
 }
 
+export function complexHonoTest() {
+  http.post('http://192.168.68.148:30008/api/v1/dive', COMPLEX_REQUEST_BODY, REQUEST_OPTIONS);
+  sleep(0.02);
+}
+
+export function complexActixTest() {
+  http.post('http://192.168.68.148:30009/api/v1/dive', COMPLEX_REQUEST_BODY, REQUEST_OPTIONS);
+  sleep(0.02);
+}
